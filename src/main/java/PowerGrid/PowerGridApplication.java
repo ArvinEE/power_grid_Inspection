18a7cb64dcfe3fa22de1ce594880953389fc5f23
1406936162@qq.com <1406936162@qq.com>
2021-04-30 17:09:27 +0800

    1、新建springboot项目，配置了逆向工程，swagger2测试，热部署等

---
+++ src/main/java/PowerGrid/PowerGridApplication.java
@@ -0,0 +1,15 @@
+package PowerGrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/** 
+* @Description: 项目启动类 
+* @Author: Zhouhao Lai
+* @Date: 2021/4/30 16:48
+*/ 
+@SpringBootApplication
+public class PowerGridApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PowerGridApplication.class,args);
+    }
+}
